4eb5ce19466d0ddb9ff1d766186363b1a6bba502
Prachi pandey <129525113+Prachi7905@users.noreply.github.com>
2023-07-14 23:15:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0b2efeab75msh8bdf412db23dbd9p181a50jsn5e9331fc8f0e',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
